752d2a96ff2f84aacd6c1bf61dd92d842bca3c6c
mrakasondara <rakasondara21@gmail.com>
2025-01-01 12:25:39 +0700

    tes endpoint

--- chat-app/src/pages/Home.jsx
+++ chat-app/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
   const [statuses, setStatuses] = useState([]);
   const statusBarProps = { statuses, setStatuses };
   const ENDPOINT = process.env.API_BASE_URL;
-
+  console.log(ENDPOINT);
   useEffect(() => {
     const fetchStatuses = async () => {
       const response = await getStatuses(userInfo.uid);
